13957210ec97012991ad63c825f62dd24dad5021
ez0603 <dlwldjs3132@naver.com>
2024-03-15 17:44:58 +0900

    로그인

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+/** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import { useInput } from '../../hooks/useInput';
 import AuthPageInput from '../../components/AuthPageInput/AuthPageInput';
 import RightTopButton from '../../components/RightTopButton/RightTopButton';
 import { Link } from 'react-router-dom';
+import { signinRequest } from '../../apis/api/signin';
 
 function SigninPage() {
     const [ username, usernameChange ] = useInput();
     const [ password, passwordChange ] = useInput();
 
+    const handleSigninSubmit = () => {
+        signinRequest({
+            username,
+            password
+        }).then(response => {
+            const accessToken = response.data;
+            localStorage.setItem("AccessToken", accessToken);
+            window.location.replace("/");
+        }).catch(error => {
+            alert(error.response.data);
+        })
+    }
+
     return (
         <>
             <div css={s.header}>
                 <h1>로그인</h1>
-                <RightTopButton onClick={null}>로그인하기</RightTopButton>
+                <RightTopButton onClick={handleSigninSubmit}>로그인하기</RightTopButton>
             </div>
             <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={usernameChange} />
             <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passwordChange} />
