a8ae36fbcdad2b5fa15dcc3aff95e8bbdfc29949
ez0603 <dlwldjs3132@naver.com>
2024-03-18 17:44:21 +0900

    240318

---
+++ src/Routes/AuthRoute.jsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { principalState } from '../atoms/principalAtom';
+import { getPrincipalRequest } from '../apis/api/principal';
+import AuthPage from '../pages/AuthPage/AuthPage';
+import HomePage from '../pages/HomePage/HomePage';
+
+function AuthRoute() {
+    const [ pirncipal, setPrincipal] = useRecoilState(principalState);
+
+    useEffect(() => {
+        getPrincipal();
+    }, []);
+
+    const getPrincipal = useCallback(() => {
+        getPrincipalRequest()
+        .then(response => {
+            setPrincipal(() => response.data);
+        }).catch(error => {
+            console.log(error);
+        });
+
+    },[]);
+
+    return (
+        <Routes>
+            <Route path='/auth/*' element={ <AuthPage /> } />
+            <Route path="/" element={ < HomePage />} />
+        </Routes>
+    );
+}
+
+export default AuthRoute;
